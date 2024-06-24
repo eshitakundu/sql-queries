615891b5b3876430aa0d69e3ffebd3f561ea0538
eshitakundu <155392114+eshitakundu@users.noreply.github.com>
2024-06-24 23:03:28 +0530

    Create schema.sql

---
+++ student/Oracle/schema.sql
@@ -0,0 +1,10 @@
+--Create a table “Student”
+CREATE TABLE Student(
+Roll Number(5),
+Name Varchar2(30),
+Age Number(5),
+Course Varchar2(5),
+Math Number(6,2),
+Physics Number(6,2),
+Computer Number(6,2),
+Birthday Date);
